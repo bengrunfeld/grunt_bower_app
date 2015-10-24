2a09957a667d01465b9edeb57751852b327583fb
bengrunfeld <benjamin.grunfeld@gmail.com>
2015-10-23 21:08:01 -0600

    update Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,21 +8,32 @@ module.exports = function(grunt) {
     //   }
     // }
     jshint: {
-      all: ['Gruntfile.js', 'dev/js/*.js']
-    }
-  });
+        all: ['Gruntfile.js', 'dev/js/*.js']
+    },
+    clean: ['dist/'],
+    copy: {
+      main: {
+        files: [
+          {expand: true, src: ['bower_components/jquery/dist/jquery.min.js'], dest: 'dist/js', filter: 'isFile'},
 
+          {expand: true, src: ['bower_components/bootstrap/dist/js/bootstrap.min.js'], dest: 'dist/js', filter: 'isFile'},
+
+          {expand: true, src: ['bower_components/bootstrap/dist/css/bootstrap.min.css'], dest: 'dist/css', filter: 'isFile'} ,
+
+          {expand: true, src: ['bower_components/bootstrap/dist/fonts/*'], dest: 'dist/fonts', filter: 'isFile'},
+        ],
+      }
+    },
+  });
+  
   // Load in Grunt's plugins
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-
 
   // Default tasks
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'clean', 'copy']);
 
 };
 
